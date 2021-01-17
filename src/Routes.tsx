23d302d55479c181b2a4915cb971d456876a0789
KevinRaleie-dev <kkevraleie@gmail.com>
2021-01-17 17:59:47 +0200

    Refactor

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 import { Nav } from './shared/Nav';
 
@@ -9,12 +10,9 @@ export const Routes = () => {
         <BrowserRouter>
             <Nav />
             <Switch>
-                <Route path='/' exact>
-                    <Home />
-                </Route>
-                <Route path="/register" exact>
-                    <Register />
-                </Route>
+                <Route path="/" exact component={Home} />
+                <Route path="/register" exact component={Register} />
+                <Route path="/login" exact component={Login} />
             </Switch>
         </BrowserRouter>
     )
