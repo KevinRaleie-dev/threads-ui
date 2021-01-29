b8b8d184ed599e9a39cc3ea1b5c630cc88d50d8c
KevinRaleie-dev <kkevraleie@gmail.com>
2021-01-29 21:22:36 +0200

    Running `me query` in nav bar

--- src/shared/Nav.tsx
+++ src/shared/Nav.tsx
@@ -1,8 +1,12 @@
-import { Flex, Box, Heading, Spacer, Button } from '@chakra-ui/react'
+import { Flex, Box, Heading, Spacer, Button, Text } from '@chakra-ui/react'
 import {Link} from 'react-router-dom';
 import React from 'react'
+import { useMeQuery } from '../generated/graphql';
 
 export const Nav = () => {
+    const {data, loading, error} = useMeQuery();
+
+    console.log('me query',data?.me);
     return (
     <Flex p={4} position='sticky' zIndex={1} top={0} backgroundColor='white' borderBottom='1px' borderBottomColor='gray.300'>
         <Box p="2">
@@ -13,18 +17,32 @@ export const Nav = () => {
             </Heading>
         </Box>
         <Spacer />
+        {!loading && error ? (<>{error?.message}</>) : 
         <Box>
-            <Link to="/register">
-                <Button colorScheme="black" bg='black' mr="4">
-                    Sign Up
-                </Button>
-            </Link>
-            <Link to='/login'>
-                <Button colorScheme="teal" variant='ghost' color='black'>
-                    Sign In         
-                </Button>
-            </Link>
+            {
+                data?.me ? (
+                    <>
+                        <Text>{data.me.username}</Text>
+                        <Button>Logout</Button>
+                    </>
+                ) : (
+                    <>
+                        <Link to="/register">
+                        <Button colorScheme="black" bg='black' mr="4">
+                            Sign Up
+                        </Button>
+                        </Link>
+                        <Link to='/login'>
+                            <Button colorScheme="teal" variant='ghost' color='black'>
+                                Sign In         
+                            </Button>
+                        </Link>
+                    </>
+                )
+            }
+           
         </Box>
+        }
     </Flex>
     )
 }
