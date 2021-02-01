425621be22f36c3ca80ecf22bef722f82b3836ab
KevinRaleie-dev <kkevraleie@gmail.com>
2021-02-01 19:11:12 +0200

    Built the not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { Box, Button, Stack, Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 export const NotFound = () => {
     return (
-        <div>
-            route not found
-        </div>
+        <>
+        <Stack spacing={2}>
+            <Text align="center" fontSize="15rem" fontWeight="700" color="gray.400">
+                404
+            </Text>
+            <Text align="center" fontWeight="500" fontSize="3xl">
+                Whoops! Sorry we could not connect the dots...
+            </Text>
+            <Text align="center" fontSize="xl">
+                This page was not found. You may have mistyped the address or the page has been moved.
+            </Text>
+            <Box 
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            >
+                <Link to='/browse'>
+                    <Button _hover={{color:'black', background: 'white', border: '2px', borderColor:'black' }} mt={5} colorScheme='black' bg='#020202'>
+                            Take me home
+                    </Button>
+                </Link>
+            </Box>
+        </Stack>
+        </>
     )
 }
