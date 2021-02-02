0e98e02c99d20b992c6c677635b79b5c4e78fc4c
KevinRaleie-dev <kkevraleie@gmail.com>
2021-02-02 14:11:23 +0200

    Redirect user to the browse page if logged in

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,26 +1,28 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Home from './pages/Home';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 import { Account } from './pages/Account';
 import { Nav } from './shared/Nav';
 import { NotFound } from './pages/NotFound';
+import { Browse } from './pages/Browse';
+import { useMeQuery } from './generated/graphql';
 
 export const Routes = () => {
+    const {data} = useMeQuery()
     return (
         <BrowserRouter>
             <Nav />
             <Switch>
-                <Route path="/" exact component={Home} />
+                <Route path="/" exact>
+                    { data?.me ? <Redirect to="/browse" /> : <Home /> }
+                </Route>
                 <Route path="/register" exact component={Register} />
                 <Route path="/login" exact component={Login} />
                 <Route path="/account/:username" exact component={Account} />
+                <Route path="/browse" exact component={Browse} />
                 <Route path="*" exact component={NotFound} />
-
-            {/* localhost:3000/account/:username -> this displays my account, store if i have one, stores i follow or people that follow my store
-                localhost:3000/account/settings
-            */}
             </Switch>
         </BrowserRouter>
     )
